e1e68f30a719134fcfea3b1bd093b9e7054bd5e8
semyo <semyoshkin8v@mail.ru>
2023-03-07 12:06:04 +0300

    DI Registrator

--- Diplom/ViewModels/DIRegistrator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Diplom.ViewModels
-{
-    public static class DIRegistrator
-    {
-        public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiBuider) =>
-            mauiBuider;
-    }
-}
