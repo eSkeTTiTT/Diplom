5e5322b46c3f5788bc39ec892072cc8ad7720539
DSemeshkin <DSemeshkin@IBS.RU>
2023-04-22 17:23:33 +0300

    Конвертеры и отображение дат

---
+++ Diplom/Views/Converters/DateTimeToDateConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Diplom.Views.Converters
+{
+	public class DateTimeToDateConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+			((DateTime)value)
+				.ToString("dd.MM.yyyy");
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+			throw new NotImplementedException();
+	}
+}
---
+++ Diplom/Views/Converters/DateTimeToYearConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Diplom.Views.Converters
+{
+    public class DateTimeToYearConverter : IMultiValueConverter
+    {
+		public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var bornDate = (DateTime?)value[0];
+			var deathdate = (DateTime?)value[1];
+
+			return string.Format("{0} - {1}",
+				bornDate?.Year,
+				deathdate?.Year);
+		}
+
+		public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture) =>
+			throw new NotImplementedException();
+	}
+}
